c574b6befdb29bba75fd2b98cf24c5695456e26d
itsurboimasarou <medicspam6@gmail.com>
2022-08-28 13:23:46 +0700

    munch: Correct kscope vendor config path

--- kscope_munch.mk
+++ kscope_munch.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Kaleidoscope stuff.
-$(call inherit-product, vendor/kscope/config/mobile.mk)
+$(call inherit-product, vendor/kscope/target/product/mobile.mk)
 
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
