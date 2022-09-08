28b1a5b9f30b7e08763e3dd2c71fd5d14854b826
itsurboimasarou <medicspam6@gmail.com>
2022-09-08 07:30:40 +0700

    munch: Add vendorsetup.sh
    Signed-off-by: itsurboimasarou <medicspam6@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+rm -rf kernel/xiaomi/munch
+git clone --depth=1 https://github.com/itsurboimasarou/kernel_xiaomi_munch kernel/xiaomi/sm8250
+
+rm -rf vendor/xiaomi
+git clone --depth=1 https://gitlab.com/itsurboimasarou/vendor_xiaomi_munch vendor/xiaomi/munch
+
+cd vendor/xiaomi
+git init
+git remote add origin https://gitlab.com/madmax7896/android_vendor_xiaomi.git
+git config core.sparseCheckout true
+echo "sm8250-common/" >> .git/info/sparse-checkout
+git pull origin lineage-19.1
