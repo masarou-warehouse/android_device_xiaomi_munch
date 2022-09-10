d3cca31d6dbb689f1ef67a4956b344c12f2850ae
itsurboimasarou <medicspam6@gmail.com>
2022-09-10 09:48:12 +0700

    munch: Clone qcom USB & hardware/xiaomi proprietaries from ArrowOS.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,9 @@
+rm -rf vendor/qcom/opensource/usb
+git clone --depth=1 https://github.com/ArrowOS/android_vendor_qcom_opensource_usb -b arrow-13.0 vendor/qcom/opensource/usb
+
+rm -rf hardware/xiaomi
+git clone --depth=1 https://github.com/ArrowOS-Devices/android_hardware_xiaomi -b arrow-13.0 hardware/xiaomi
+
 rm -rf device/xiaomi/sm8250-common
 git clone --depth=1 https://github.com/itsurboimasarou/device_xiaomi_sm8250-common device/xiaomi/sm8250-common
 
@@ -13,3 +19,5 @@ git remote add origin https://gitlab.com/madmax7896/android_vendor_xiaomi.git
 git config core.sparseCheckout true
 echo "sm8250-common/" >> .git/info/sparse-checkout
 git pull origin lineage-19.1
+
+cd ../..
